6c4b0c54a4b7aa8e53c325f6fcb04161fe241e26
Nathan Billings <billings90@gmail.com>
2021-08-29 15:02:29 -0400

    initial commit with file hash function

---
+++ dedupy.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+import os, hashlib
+
+# Currently errors on denied permissions
+def hash_files(path):
+    '''Returns an MD5 hash for all files in a given path.'''
+
+    hasher = hashlib.md5()
+
+    if os.path.isdir(path) == True:
+
+        path_list = []
+        symlink_list = []
+        problem_list = []
+        file_hash_dict = {}
+        files = os.listdir(path)
+
+        for file in files:
+
+            full_path = os.path.join(path, file)
+            
+            if os.path.islink(full_path):
+                symlink_list.append(full_path)
+            elif os.path.isdir(full_path):
+                path_list.append(full_path)
+            elif not os.path.isdir(full_path):
+                with open(full_path, 'rb') as prehash_file:
+                    buffer = prehash_file.read()
+                    hasher.update(buffer)
+                file_hash_dict.update({full_path: hasher.hexdigest()})
+            else:
+                problem_list.append(full_path)
+
+    for file_path, file_hash in file_hash_dict.items():
+        print("{} is the hash of {}".format(file_hash, file_path))
+
+    return file_hash_dict
+
+hash_files("/bin/")
